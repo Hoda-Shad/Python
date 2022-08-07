ea08b9c26995483faf078aca58eaf9f135b4d8ce
Hoda Shad <99975022+Hoda-Shad@users.noreply.github.com>
2022-08-07 16:19:50 +0430

    Add files via upload

---
+++ Assignment18/Qt-Guess/guess.py
@@ -0,0 +1,41 @@
+import random
+
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+from PySide6.QtUiTools import QUiLoader
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        loader = QUiLoader()
+        self.ui = loader.load('guess.ui')
+        self.ui.show()
+        self.ui.setWindowTitle('Guess Game')
+        self.ui.check_btn.clicked.connect(self.a)
+        self.ui.new_btn.clicked.connect(self.b)
+        self.num = random.randint(0, 100)
+
+
+    def a(self):
+        guess_num = int(self.ui.lineEdit.text())
+        if self.num > guess_num :
+            msgBox = QMessageBox()
+            msgBox.setText('برو بالاتر')
+            msgBox.exec()
+
+        elif self.num < guess_num:
+            msgBox = QMessageBox()
+            msgBox.setText('برو پایین تر')
+            msgBox.exec()
+        else:
+            msgBox = QMessageBox()
+            msgBox.setText('برنده شدی')
+            msgBox.exec()
+
+    def b(self):
+        self.ui.lineEdit.setText('')
+
+app = QApplication([])
+win = MainWindow()
+app.exec()
---
+++ Assignment18/Qt-Sodoku/readme.md
@@ -0,0 +1,21 @@
+### Sudoku Game with Python and Pyside6
+
+----
+
+Options:
+
+1- Checking the new entry immediately
+
+![](1.png)
+
+2- Error when the Sudoku text file is not find
+
+![](4.png)
+
+3- Dark mode Button
+
+![](3.png)
+
+4- Congratulate when the user wins
+
+![](win.png)
